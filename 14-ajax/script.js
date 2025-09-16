425b9220837c855e911a62c537513b22f00b02a0
Alif-Kopling <2425307.alif@smkn-2sbg.sch.id>
2025-09-16 19:09:27 +0700

    feat: 14 ajax

---
+++ 14-ajax/script.js
@@ -0,0 +1,12 @@
+// Contoh AJAX
+
+function loadData() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("hasil").innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET", "data.txt", true);
+    xhttp.send();
+}
