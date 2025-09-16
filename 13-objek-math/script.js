dcf91fd4ffa411a6298fb443bf336264102d7129
Alif-Kopling <2425307.alif@smkn-2sbg.sch.id>
2025-09-16 19:09:27 +0700

    feat: 13 objek math

---
+++ 13-objek-math/script.js
@@ -0,0 +1,7 @@
+// Contoh objek Math
+
+console.log(Math.PI); // 3.141592653589793
+console.log(Math.abs(-4.7)); // 4.7
+console.log(Math.pow(8, 2)); // 64
+console.log(Math.sqrt(64)); // 8
+console.log(Math.max(1, 3, 5)); // 5
