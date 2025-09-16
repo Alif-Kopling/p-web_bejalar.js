014cc944f8bdebaf8f54c7c0696e2bfebd6491a0
Alif-Kopling <2425307.alif@smkn-2sbg.sch.id>
2025-09-16 19:09:26 +0700

    feat: 09 struktur data array

---
+++ 09-struktur-data-array/script.js
@@ -0,0 +1,19 @@
+// Contoh struktur data array
+
+let buah = ["Apel", "Jeruk", "Mangga"];
+
+// Mengakses elemen array
+console.log(buah[0]); // Apel
+
+// Menambah elemen ke array
+buah.push("Durian");
+console.log(buah);
+
+// Menghapus elemen dari array
+buah.pop();
+console.log(buah);
+
+// Looping array
+for (let i = 0; i < buah.length; i++) {
+    console.log(buah[i]);
+}
