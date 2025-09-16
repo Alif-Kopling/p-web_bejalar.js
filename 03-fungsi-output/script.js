5bc10b4539a2fa7aa3eaf04a23c7b54064ac9e36
Alif-Kopling <2425307.alif@smkn-2sbg.sch.id>
2025-09-16 19:09:25 +0700

    feat: 03 fungsi output

---
+++ 03-fungsi-output/script.js
@@ -0,0 +1,10 @@
+// Contoh fungsi output
+
+// 1. alert()
+alert("Ini adalah alert!");
+
+// 2. console.log()
+console.log("Ini adalah console.log!");
+
+// 3. document.write()
+document.write("<p>Ini adalah document.write()</p>");
