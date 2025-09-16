17daa805d60eb25a4cf3580365f07d0b6087ce5f
Alif-Kopling <2425307.alif@smkn-2sbg.sch.id>
2025-09-16 19:09:25 +0700

    feat: 04 variabel dan tipe data

---
+++ 04-variabel-dan-tipe-data/script.js
@@ -0,0 +1,24 @@
+// Contoh variabel dan tipe data
+
+// String
+let nama = "Budi";
+console.log("Nama: " + nama);
+
+// Number
+let umur = 25;
+console.log("Umur: " + umur);
+
+// Boolean
+let sudahMenikah = false;
+console.log("Sudah Menikah: " + sudahMenikah);
+
+// Array
+let hobi = ["membaca", "menulis", "bermain game"];
+console.log("Hobi: " + hobi[0]);
+
+// Object
+let alamat = {
+    jalan: "Jl. Pahlawan No. 10",
+    kota: "Bandung"
+};
+console.log("Alamat: " + alamat.jalan + ", " + alamat.kota);
