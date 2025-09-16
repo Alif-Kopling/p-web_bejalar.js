e8d873a6f4480a5e65b2a7594a5cf03906ed1df1
Alif-Kopling <2425307.alif@smkn-2sbg.sch.id>
2025-09-16 19:09:26 +0700

    feat: 07 percabangan

---
+++ 07-percabangan/script.js
@@ -0,0 +1,22 @@
+// Contoh percabangan
+
+// if-else
+let nilai = 75;
+if (nilai >= 70) {
+    console.log("Anda lulus!");
+} else {
+    console.log("Anda tidak lulus.");
+}
+
+// switch-case
+let hari = "Senin";
+switch (hari) {
+    case "Senin":
+        console.log("Hari ini hari Senin.");
+        break;
+    case "Selasa":
+        console.log("Hari ini hari Selasa.");
+        break;
+    default:
+        console.log("Hari ini bukan hari Senin atau Selasa.");
+}
