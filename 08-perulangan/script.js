995f2954b533a4de19d62c0627cfc057dcf0d13a
Alif-Kopling <2425307.alif@smkn-2sbg.sch.id>
2025-09-16 19:09:26 +0700

    feat: 08 perulangan

---
+++ 08-perulangan/script.js
@@ -0,0 +1,20 @@
+// Contoh perulangan
+
+// for loop
+for (let i = 0; i < 5; i++) {
+    console.log("Iterasi ke-" + i);
+}
+
+// while loop
+let j = 0;
+while (j < 5) {
+    console.log("Iterasi ke-" + j);
+    j++;
+}
+
+// do-while loop
+let k = 0;
+do {
+    console.log("Iterasi ke-" + k);
+    k++;
+} while (k < 5);
