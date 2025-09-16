534621e65b1599c0369d85bf565971ebff91399b
Alif-Kopling <2425307.alif@smkn-2sbg.sch.id>
2025-09-16 19:09:26 +0700

    feat: 06 operator

---
+++ 06-operator/script.js
@@ -0,0 +1,28 @@
+// Contoh operator
+
+// Operator Aritmatika
+let a = 10;
+let b = 5;
+console.log("a + b = " + (a + b));
+console.log("a - b = " + (a - b));
+console.log("a * b = " + (a * b));
+console.log("a / b = " + (a / b));
+console.log("a % b = " + (a % b));
+
+// Operator Penugasan
+let c = 10;
+c += 5; // c = c + 5
+console.log("c = " + c);
+
+// Operator Perbandingan
+let d = 10;
+let e = "10";
+console.log("d == e: " + (d == e)); // true
+console.log("d === e: " + (d === e)); // false
+
+// Operator Logika
+let f = true;
+let g = false;
+console.log("f && g: " + (f && g)); // false
+console.log("f || g: " + (f || g)); // true
+console.log("!f: " + !f); // false
