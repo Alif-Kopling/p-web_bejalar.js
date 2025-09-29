ae4b7b68da40e058c1443a865ff2c61985d3ca33
Alif-Kopling <2425307.alif@smkn-2sbg.sch.id>
2025-09-29 11:24:20 +0700

    Perulangan

--- 08-perulangan/script.js
+++ 08-perulangan/script.js
@@ -2,19 +2,19 @@
 
 // for loop
 for (let i = 0; i < 5; i++) {
-    console.log("Iterasi ke-" + i);
+    console.log("jawir ke-" + i);
 }
 
 // while loop
 let j = 0;
 while (j < 5) {
-    console.log("Iterasi ke-" + j);
+    console.log("hihir ke-" + j);
     j++;
 }
 
 // do-while loop
 let k = 0;
 do {
-    console.log("Iterasi ke-" + k);
+    console.log("hihir ke-" + k);
     k++;
 } while (k < 5);
