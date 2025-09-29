21953f0ece5e9c2ca9cb5002f16ba90374ede8b1
Alif-Kopling <2425307.alif@smkn-2sbg.sch.id>
2025-09-29 10:42:21 +0700

    Fungsi Output di Javascript

--- 03-fungsi-output/script.js
+++ 03-fungsi-output/script.js
@@ -4,7 +4,7 @@
 alert("Ini adalah alert!");
 
 // 2. console.log()
-console.log("Ini adalah console.log!");
+console.log("Ini adalah console.log");
 
 // 3. document.write()
 document.write("<p>Ini adalah document.write()</p>");
